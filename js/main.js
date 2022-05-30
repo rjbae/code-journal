0879026533c47182575cc8dd75a88c254c9f46ad
Ryan Bae <baeryan21@gmail.com>
2022-05-30 22:20:13 +0000

    Listened for submit events on form and completed sub tasks underneath.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-var photoUrl = document.querySelector('#photourl');
-var placeHolderImg = document.querySelector('.placeholder-img');
+var $photoUrl = document.querySelector('.photo-url');
+var $placeHolderImg = document.querySelector('.placeholder-img');
 
-photoUrl.addEventListener('input', updatePhoto);
+$photoUrl.addEventListener('input', updatePhoto);
 
 function updatePhoto(event) {
   var newUrl = event.target.value;
-  placeHolderImg.setAttribute('src', newUrl);
+  $placeHolderImg.setAttribute('src', newUrl);
+}
+
+var $form = document.querySelector('form');
+$form.addEventListener('submit', formEvent);
+
+function formEvent(event) {
+  event.preventDefaul();
+  var formEntry = {};
+  formEntry.title = $form.elements.title.value;
+  formEntry.photoUrl = $form.elements.photoUrl.value;
+  formEntry.notes = $form.elements.notes.value;
+  formEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(formEntry);
+  $placeHolderImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 }
